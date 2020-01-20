6fba7ae4e4cb19b152930b133d8b474a083e8004
3 4 T <40402038+ballad4tears@users.noreply.github.com>
2020-01-20 14:24:05 +0700

    แม่ส่งไปหาลูก ลูกส่งไปหาแม่ ลูกส่งไปหาลูก

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,24 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
+
 })
 export class AppComponent {
   title = 'comunication';
+  count = 0;
+
+  onClickCount(){
+  this.count += 1;
 }
+
+  onClickReset(val){
+  this.count = val;
+}
+
+onClickReset2(val){
+  this.count = val;
+}
+
+}
+
--- src/app/share/communicate.service.ts
+++ src/app/share/communicate.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CommunicateService {
-
+  count = 0
   constructor() { }
 }
